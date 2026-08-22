c541ff85319abf1f732126315bad8ee75229d8af
Wender Rodrigo de Oliveira Alves <156224418+dev-guilherme-alves@users.noreply.github.com>
2026-08-21 22:07:55 -0300

    Tipos Básicos, Tupla e Lista de Tupla

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+// Tipos Básicos
+let age: number = 5;
+const firstName: string = 'Guilherme'
+const isValid: boolean = true
+let idk: any = 5
+
+idk ='12'
+idk = true
+
+const ids: number[] = [1, 2, 3, 4, 5];
+const booleans: boolean[] = [true, false, true, false]
+const names: string[] = ['Guilherme', 'Jane']
+
+// Tupla
+const person: [number, string] = [1, 'Jane']
+
+// Lista de Tuplas
+const people: [number, string][] = [
+    [1, 'Jane']
+    [2, 'Doe']
+];
+
+// Intersections
+const productId: string | number | boolean = false
+
+// Enum
+
+
+console.log(age);
