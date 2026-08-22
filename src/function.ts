f7a76dbb61468b012e3f175878253f0ba6717b73
Wender Rodrigo de Oliveira Alves <156224418+dev-guilherme-alves@users.noreply.github.com>
2026-08-22 19:08:53 -0300

    Interfaces

--- src/function.ts
+++ src/function.ts
@@ -1,8 +1,16 @@
-const sun = (x: number, y: number): string | number => {
-    return (x + y).toString();
+interface MathFunc {
+    (x: number, y: number): number;
+}
+
+const sum = (x: number, y: number): number => {
+    return x + y;
 };
 
-const value = sun(2, 3);
+const sub: MathFunc = (x : number, y: number) => {
+    return x - y;
+};
+
+const value = sum(2, 3);
 
 const log = (message: string): void => {
     console.log(message);
--- src/objects.ts
+++ src/objects.ts
@@ -16,7 +16,7 @@ const user: User = {
     fistName: 'jane',
     age: 20,
     mail: 'jane@doe.com',
-    orders: [{ productId: '1', price: 200}],
+    orders: [{ productId: 1, price: 2}],
 };
 
 const printLog = (message: string) => {}
@@ -32,5 +32,30 @@ const author: Author & User = {
     age: 2,
     books: ['1', '2'],
     mail: 'gama.2@mail.com',
-    fistName: 'Guilherme'
+    fistName: 'Guilherme',
+    orders: [],
+};
+
+// Interfaces
+interface UserInterface {
+    readonly firstName: string;
+    email: string;
 }
+
+const emailUser: UserInterface = {
+    email: "Jane@gmail.com",
+    firstName: "Jane",
+};
+
+interface AuthorInterface {
+    books: string[];
+}
+
+const newAuthor: UserInterface & AuthorInterface = {
+    email: "@gmail.com",
+    firstName: "Does",
+    books: ["3", "4"]
+};
+
+type Grade = number | string;
+const grade: Grade = 1;
